b53d3d8a016862b0e49cd336eef57228071a3a92
Harshal141 <harshalmukundapatil@gmail.com>
2023-09-15 23:22:27 +0530

    concurrent package

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// simple strater check code
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      TODO: Here i want an md editor
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const multer = require('multer');
 
 // Create a variable to hold the custom filename
