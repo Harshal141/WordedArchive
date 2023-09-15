a7d027362422f63b6fd91b64d738a1080613fa94
Harshal141 <harshalmukundapatil@gmail.com>
2023-09-15 23:04:46 +0530

    custom name

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const multer = require('multer');
 
-// Require the upload middleware
-const upload = require('./upload');
+// Create a variable to hold the custom filename
+let customFilename = '';
+
+// Set up storage for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    // Use the customFilename variable as the filename
+    cb(null, customFilename+file.originalname);
+  }
+});
+
+// Create the multer instance
+const upload = multer({ storage: storage });
 
 // render the index page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // Set up a route for file uploads
-app.post('/upload', upload.single('file'), (req, res) => {
-  // Handle the uploaded file
-  res.json({ message: 'File uploaded successfully!' });
+app.post('/upload', (req, res) => {
+  // Set the customFilename based on your POST request variable
+  customFilename = Date.now() + '-' + 'req.body.myVar' + '-';
+
+  // Use the upload middleware to handle the file upload
+  upload.single('file')(req, res, (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ message: 'File uploaded successfully!' });
+  });
 });
 
 app.listen(port, () => {
