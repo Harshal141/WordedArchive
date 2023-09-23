41a3b8859f685054b61848496e024664878df781
Harshal141 <harshalmukundapatil@gmail.com>
2023-09-23 22:40:17 +0530

    nav and ui update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import MDEditor from '@uiw/react-md-editor';
 import rehypeSanitize from "rehype-sanitize";
+import "./App.css";
+import {Navbar} from "./components/navbar";
 
 export default function App() {
-  const [value, setValue] = React.useState(`**Hello world!!!**`);
+  const [value, setValue] = React.useState(`👋 **Hello!** You can edit the .md code here`);
+
   return (
-    <div className="container">
+    <>
+      <Navbar />
+      <div className="m-4">
       <MDEditor
         value={value}
         onChange={setValue}
@@ -13,6 +18,7 @@ export default function App() {
           rehypePlugins: [[rehypeSanitize]],
         }}
       />
-    </div>
+      </div>
+    </>
   );
 }
