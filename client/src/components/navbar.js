61447ebe01694c527d3d33c3b73c22695c0432b2
guntarakesh <guntarakesh971@gmail.com>
2023-10-06 16:46:54 +0530

    Enhancement of Navbar-2

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,34 +1,33 @@
 import React, { useState } from 'react'
 import { Link, useSearchParams } from 'react-router-dom';
-import png from './icons8-male-user-94.png'
 
-import obook from './open-book.png'
-import cbook from './close-book.png'
-
-import dashboard1 from './icons8-menu-40.png'
+import png from '../Asserts/user-icon.png'
+import obook from '../Asserts/open-book.png'
+import cbook from '../Asserts/close-book.png'
+import dashboard1 from '../Asserts/dashboard-icon.png'
 
 const Navbar = () => {
 
-  const [tr, setTr] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
-  const handleClick = () => {
-    setTr(!tr);
+  const handleClicktoChangeNavbar = () => {
+    setIsClicked(!isClicked);
   }
 
   return (
     <div className='rgnav'>
-      <div className='rgtoggle' style={{ left: tr ? '0px' : '-70%' }}>
+      <div className='rgtoggle' style={{ left: isClicked ? '0px' : '-70%' }}>
       <button className='rgdashboard' style={{ backgroundColor: '#3973ca'}}> <Link style={{ display: 'flex' , alignItems: 'center' }}> <img id='rgdashboardIcon' src={dashboard1} /> Dashboard </Link>  </button>
           <button className='rgdashboard' style={{ backgroundColor: '#3973ca' }}> <Link href='/upload'> &#x270e;
-            Upload Pdf</Link>  <i class='fas fa-arrow-circle-up'></i> </button>
+            Upload Pdf</Link>   </button>
           <button className='rgdashboard'> <Link href='/community'>Explore</Link> </button>
       </div>
       <div className='navpart1'>
-        <img id='navbar-toggle' onClick={handleClick} style={{ width: '30px' }} src={tr ? obook : cbook} />
+        <img id='navbar-toggle' onClick={handleClicktoChangeNavbar} style={{ width: '30px' }} src={isClicked ? obook : cbook} />
         <div className='rglinks'>
           <button className='rgdashboard' style={{ backgroundColor: '#3973ca'}}> <Link style={{ display: 'flex' , alignItems: 'center' }}> <img id='rgdashboardIcon' src={dashboard1} /> Dashboard </Link>  </button>
           <button className='rgdashboard' style={{ backgroundColor: '#3973ca' }}> <Link href='/upload'> &#x270e;
-            Upload Pdf</Link>  <i class='fas fa-arrow-circle-up'></i> </button>
+            Upload Pdf</Link>  </button>
           <button className='rgdashboard'> <Link href='/community'>Explore</Link> </button>
         </div>
 
